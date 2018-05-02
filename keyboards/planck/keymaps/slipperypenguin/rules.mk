8f84614152f169042d65f09e122ed67398bd2c58
Brady Paterson <brady@odicro.com>
2018-05-02 00:05:20 -0400

    Initial setup
    initial setup for my keyboard folder

---
+++ keyboards/planck/keymaps/slipperypenguin/rules.mk
@@ -0,0 +1,14 @@
+# Build Options
+BACKLIGHT_ENABLE = yes      # Enable keyboard backlight
+
+
+#MIDI_ENABLE = no            # MIDI controls
+#AUDIO_ENABLE = no           # Audio output on port C6
+#UNICODE_ENABLE = no         # Unicode
+#BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+#RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
+
+
+#ifndef QUANTUM_DIR
+#	include ../../../../Makefile
+#endif
